b84d51ab5333cb0a6fe8b675d311838800399da8
Somanshu Bhardwaj <89822689+somanshubhardwaj@users.noreply.github.com>
2024-09-14 13:11:00 +0530

    added links

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
-      <span className="font-code text-3xl font-bold">Pixonoids</span>
+      <Link to="/">
+        <span className="font-code text-3xl font-bold">Pixonoids</span>
+      </Link>
     </div>
   );
 };
